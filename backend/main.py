32939adc90ec440d6b2184ed676d2911a7df766c
promise-temi <temi.promisejohn@gmail.com>
2025-10-24 00:41:29 +0200

    mail

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,22 @@
 from flask import Flask, jsonify, request, session
 from flask_cors import CORS
+from flask_mail import Mail, Message
+import os
 import random
 import os
 
 
 app = Flask(__name__)
 
+app.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT=587,
+    MAIL_USE_TLS=True,
+    MAIL_USERNAME=os.getenv('MAIL_USERNAME'),      
+    MAIL_PASSWORD=os.getenv('MAIL_PASSWORD'),      
+    MAIL_DEFAULT_SENDER=os.getenv('MAIL_USERNAME')
+)
+mail = Mail(app)
 
 
 cors = CORS(app,resources={r"/api/*": {"origins": ["http://localhost:3000", "http://127.0.0.1:3000"]}})
@@ -43,6 +54,14 @@ def send_message():
 
     if int(data['captcha']) == cAPTCHA:
         print(data)
+        # Dans /api/send_message, après captcha OK :
+        msg = Message(
+            subject=data.get('subject','Test'),
+            recipients=[os.getenv('MAIL_USERNAME')],       # à toi-même
+            body=data.get('message','(vide)')
+        )
+        mail.send(msg)
+
         return jsonify(message="Tout fonctionne parfaitement"), 200
     else:
         return jsonify(message="aie..."), 500
