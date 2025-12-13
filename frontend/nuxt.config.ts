2e014a715ff1262e48ef215a5a41787ea3c70956
promise-temi <temi.promisejohn@gmail.com>
2025-12-13 18:10:04 +0100

    .

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   modules: ['@nuxt/icon', '@vueuse/motion/nuxt'],
 
   app: {
-    baseURL: '/',
+    baseURL: '/portofolio-nuxt/',
     head: {
       link: [
         {
